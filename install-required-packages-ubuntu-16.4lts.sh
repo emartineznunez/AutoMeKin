3baca41767853b3c8555f6e6c0a209e435cc2dfe
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2024-03-06 07:31:00 +0100

    Update install-required-packages-ubuntu-16.4lts.sh

--- install-required-packages-ubuntu-16.4lts.sh
+++ install-required-packages-ubuntu-16.4lts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install bc environment-modules gawk gcc gfortran parallel sqlite3 zenity gnuplot
+sudo apt install bc environment-modules gawk gcc gfortran parallel sqlite3 gnuplot
