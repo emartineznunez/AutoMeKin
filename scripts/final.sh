fc4b7dcc4239aee735671ca66f7eb2e690c1eb3a
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2024-03-07 07:37:18 +0100

    Update final.sh

--- scripts/final.sh
+++ scripts/final.sh
@@ -379,7 +379,7 @@ cd ${final}
 #fi
 
 #####################################################^
-rm -rf population${postb}.gnu 
+rm -rf population${postb}.gnu pop_data*
 #####################################################v
 if ! [ -z "$AMK_REPORT" ]
 then
